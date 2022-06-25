f556c5dfbd9f4a2655f2823239190b4d63162f34
rishavcodes <rjnycusa397@gmail.com>
2022-06-25 11:53:33 +0530

    Updates

---
+++ precache-manifest.d42b79ffa58794fdbe5980f3a9f838d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4934e490261788d950ae",
+    "url": "/dashboardapp/static/css/main.11610cb2.chunk.css"
+  },
+  {
+    "revision": "4934e490261788d950ae",
+    "url": "/dashboardapp/static/js/main.4934e490.chunk.js"
+  },
+  {
+    "revision": "711d6637607d5bf510fa",
+    "url": "/dashboardapp/static/js/1.711d6637.chunk.js"
+  },
+  {
+    "revision": "c2344180bc47d540cc72",
+    "url": "/dashboardapp/static/js/runtime~main.c2344180.js"
+  },
+  {
+    "revision": "a340da6a689b0177f5e910a88016d98a",
+    "url": "/dashboardapp/index.html"
+  }
+];
