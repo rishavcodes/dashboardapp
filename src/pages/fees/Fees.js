382202d6d2ff86b51082834093c30432ead4e2b4
Rishav jha <72191032+rishavcodes@users.noreply.github.com>
2022-06-26 16:48:52 +0530

    Update Fees.js

--- src/pages/fees/Fees.js
+++ src/pages/fees/Fees.js
@@ -3,7 +3,28 @@ import React from 'react';
 function Fees() {
   return (
     <div>
-      
+      <h1 className='feeListTitle'>Fees </h1>
+      <table className='feeTable'>
+        <thead>
+          <th  className='listItem'>Semester</th>
+          <th  className='listItem'>Semester Start Date</th>
+          <th  className='listItem'>Semester End Date</th>
+          <th  className='listItem'>Passing-Status</th>
+          <th  className='listItem'>Fee Status</th>
+        </thead>
+        <tbody>
+          {data.map((d)=>(
+            <tr key={d.id}>
+              <td className='listItem'>{d.Semester}</td>
+              <td className='listItem'>{d.Semester_Start_Date}</td>
+              <td className='listItem'>{d.Semester_End_Date}</td>
+              <td className='listItem'>{d.Passing_status}</td>
+              <td className='listItem'>{d.Status}</td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
